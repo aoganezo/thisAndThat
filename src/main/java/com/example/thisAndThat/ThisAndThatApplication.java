9d91a3dabbb1e07d4675950ea7ad5eb8ab98a8f4
artur oganezov <arthur.oganezov@gmail.com>
2021-01-11 19:41:23 -0600

    main class

---
+++ src/main/java/com/example/thisAndThat/ThisAndThatApplication.java
@@ -0,0 +1,13 @@
+package com.example.thisAndThat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisAndThatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisAndThatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thisAndThat/ThisAndThatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thisAndThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThisAndThatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
